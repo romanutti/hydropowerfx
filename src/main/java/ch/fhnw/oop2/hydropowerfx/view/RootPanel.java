7d8783d09983b975a7ab71eaff388fa0c66914fe
mgerboth <malte.gerboth@gmail.com>
2018-04-24 15:10:31 +0200

    Inital creation CantonList Class

--- src/main/java/ch/fhnw/oop2/hydropowerfx/view/RootPanel.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/view/RootPanel.java
@@ -1,39 +1,48 @@
 package ch.fhnw.oop2.hydropowerfx.view;
 
+import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.StackPane;
 
 import ch.fhnw.oop2.hydropowerfx.presentationmodel.RootPM;
 
-public class RootPanel extends SplitPane implements ViewMixin {
+public class RootPanel extends StackPane implements ViewMixin {
     private final RootPM rootPM;
 
-    private Button button;
+    private Button button1;
 
     public RootPanel(RootPM model) {
         this.rootPM = model;
-
+        initializeSelf();
+        initializeControls();
+        layoutControls();
+        setupBindings();
         init();
     }
 
     @Override
     public void initializeSelf() {
-        addStylesheetFiles("style.css");
+        String stylesheet = getClass().getResource("style.css").toExternalForm();
+        getStylesheets().add(stylesheet);
     }
 
     @Override
     public void initializeControls() {
-        button = new Button();
+        Details tmp = new Details();
+        button1 = new Button();
+
     }
 
     @Override
     public void layoutControls() {
-        getChildren().add(button);
+
+        getChildren().add(button1);
+
     }
 
     @Override
     public void setupBindings() {
-        button.textProperty().bind(rootPM.greetingProperty());
+        button1.textProperty().bind(rootPM.greetingProperty());
     }
 }
